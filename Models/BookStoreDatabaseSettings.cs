1bb570bdcac4e3685f985cb2f7b1fe639ff9eaa4
andrevpc <andrevictor06@gmail.com>
2023-08-03 16:49:50 -0300

    mongoDB

---
+++ Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,8 @@
+namespace mongoConnection.Models;
+
+public class BookStoreDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set;} = null!;
+    public string BookCollectionName { get; set;} = null!;
+}
